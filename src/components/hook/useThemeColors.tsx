8edf82a40383898973a645f84cbf55f00f7c60a2
Justine George <justinegeo96@gmail.com>
2023-08-18 15:19:31 -0500

    update theme colors

--- src/components/hook/useThemeColors.tsx
+++ src/components/hook/useThemeColors.tsx
@@ -2,37 +2,39 @@ import { useSelector } from "react-redux";
 import { RootState } from "../../redux/store";
 
 export function useThemeColors() {
-  const settingsData = useSelector((state: RootState) => state.settingsDataState);
+  const settingsData = useSelector(
+    (state: RootState) => state.settingsDataState
+  );
 
   return settingsData.isDarkMode
     ? {
-        PRIMARY_COLOR: "#1F1F1F",
-        SECONDARY_COLOR: "#353535",
-        SELECTION_COLOR: "#575757",
-        HOVER_COLOR: "#353535",
-        ICON_HOVER_COLOR: "#000000",
-        DELETE_ICON_HOVER_COLOR: "#FF5733",
-        LABEL_L3_COLOR: "#7F7F7F",
-        LABEL_L1_COLOR: "#D2D2D2",
-        LABEL_L2_COLOR: "#B5B5B5",
-        TAG_BG_COLOR: "#2C2C2C",
-        TAG_BORDER_COLOR: "#3F3F3F",
-        BORDER_COLOR: "#FFFFFF",
-        TEXT_COLOR: "#FFFFFF",
+        PRIMARY_COLOR: "#252839",
+        SECONDARY_COLOR: "#2C2F3E",
+        SELECTION_COLOR: "#383B52",
+        HOVER_COLOR: "#343648",
+        ICON_HOVER_COLOR: "#1B1D28",
+        DELETE_ICON_HOVER_COLOR: "#FF6347",
+        LABEL_L3_COLOR: "#6F728A",
+        LABEL_L1_COLOR: "#C0C2CE",
+        LABEL_L2_COLOR: "#9A9BB2",
+        TAG_BG_COLOR: "#292B3C",
+        TAG_BORDER_COLOR: "#1E202B",
+        BORDER_COLOR: "#DADCE4",
+        TEXT_COLOR: "#EDEFF2",
       }
     : {
-        PRIMARY_COLOR: "#E0E0E0",
-        SECONDARY_COLOR: "#CACACA",
-        SELECTION_COLOR: "#A8A8A8",
-        HOVER_COLOR: "#CACACA",
-        ICON_HOVER_COLOR: "#FFFFFF",
-        DELETE_ICON_HOVER_COLOR: "#FF5733",
-        LABEL_L3_COLOR: "#808080",
-        LABEL_L1_COLOR: "#2D2D2D",
-        LABEL_L2_COLOR: "#4A4A4A",
-        TAG_BG_COLOR: "#D3D3D3",
-        TAG_BORDER_COLOR: "#C0C0C0",
-        BORDER_COLOR: "#000000",
-        TEXT_COLOR: "#000000",
+        PRIMARY_COLOR: "#F0F2F5",
+        SECONDARY_COLOR: "#E1E4E8",
+        SELECTION_COLOR: "#C2C5CC",
+        HOVER_COLOR: "#D9DCDF",
+        ICON_HOVER_COLOR: "#F5F7FA",
+        DELETE_ICON_HOVER_COLOR: "#FF6347",
+        LABEL_L3_COLOR: "#74777E",
+        LABEL_L1_COLOR: "#2E3136",
+        LABEL_L2_COLOR: "#505357",
+        TAG_BG_COLOR: "#E9EBED",
+        TAG_BORDER_COLOR: "#D4D6D9",
+        BORDER_COLOR: "#2E3136",
+        TEXT_COLOR: "#1A1B1E",
       };
 }
